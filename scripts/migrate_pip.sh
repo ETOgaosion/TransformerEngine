533e6d8bfc57c3a9d4aa0d2d92e798faa0a14b6d
BlueSpace <gaoziyuan19@mails.ucas.ac.cn>
2024-12-14 07:26:02 +0000

    add pip transfer scripts

---
+++ scripts/migrate_pip.sh
@@ -0,0 +1,5 @@
+cp -r /workspace/TransformerEngine/build .
+cp -r /workspace/TransformerEngine/libtransformer_engine.so .
+cp -r /workspace/TransformerEngine/transformer_engine.egg-info
+cp -r /workspace/TransformerEngine/transformer_engine_torch.cpython-310-x86_64-linux-gnu.so
+cp -r /workspace/TransformerEngine/libtransformer_engine.so
